3aaa6b3e987e94e409de9f96800caa7cb9ccba1a
bo0tzz <btdewilligen@gmail.com>
2022-05-04 13:54:03 +0200

    Move footer script into html

--- main.js
+++
@@ -1,9 +0,0 @@
-const gifs = [
-    "assets/img/ded.gif",
-    "assets/img/yay.gif",
-    "assets/img/angel.gif"
-]
-
-window.addEventListener('load', ev => {
-    document.getElementById("footer-gif").src = gifs[Math.floor(Math.random() * gifs.length)]
-})
